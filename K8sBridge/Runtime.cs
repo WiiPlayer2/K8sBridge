4d281e8d84a01742fb363812cbb129cc1ad5ce71
Waldemar Tomme <waldemar.tomme@bluehands.de>
2023-08-30 14:13:35 +0200

    Add runtime

---
+++ K8sBridge/Runtime.cs
@@ -0,0 +1,56 @@
+﻿using K8sBridge.Application.Abstractions;
+using K8sBridge.Application.Traits;
+
+internal readonly struct Runtime :
+    HasCancel<Runtime>,
+    HasApp<Runtime>
+{
+    private readonly Env_? env;
+
+    private Runtime(Env_ env)
+    {
+        this.env = env;
+    }
+
+    public Env_ Env => env ?? throw new InvalidOperationException();
+
+    public Eff<Runtime, IKubernetesApi> KubernetesApiEff => Env.KubernetesApiEff;
+
+    public Eff<Runtime, ITunnelingApi> TunnelingApiEff => Env.TunnelingApiEff;
+
+    public CancellationToken CancellationToken => Env.CancellationToken;
+
+    public CancellationTokenSource CancellationTokenSource => Env.CancellationTokenSource;
+
+    public Runtime LocalCancel =>
+        Env.Apply(env =>
+        {
+            var cts = CancellationTokenSource.CreateLinkedTokenSource(env.CancellationToken);
+            return New(env with
+            {
+                CancellationToken = cts.Token,
+                CancellationTokenSource = cts,
+            });
+        });
+
+    public static Runtime New(Env_ env) => new(env);
+
+    public static Runtime New(
+        CancellationToken cancellationToken,
+        IKubernetesApi kubernetesApi,
+        ITunnelingApi tunnelingApi)
+    {
+        var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+        return New(new Env_(
+            cts.Token,
+            cts,
+            SuccessEff(kubernetesApi),
+            SuccessEff(tunnelingApi)));
+    }
+
+    public record Env_(
+        CancellationToken CancellationToken,
+        CancellationTokenSource CancellationTokenSource,
+        Eff<Runtime, IKubernetesApi> KubernetesApiEff,
+        Eff<Runtime, ITunnelingApi> TunnelingApiEff);
+}
