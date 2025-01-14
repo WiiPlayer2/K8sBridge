5f7dcf2d821673f9f40a04359e71d2d5e8d0f5dc
Waldemar Tomme <waldemar.tomme@bluehands.de>
2025-01-14 15:13:28 +0100

    Fix package building

--- package.nix
+++ package.nix
@@ -12,6 +12,8 @@ buildDotnetModule rec {
   };
 
   projectFile = "K8sBridge/K8sBridge.csproj";
+  nugetDeps = ./deps.json; # nix build .#k8s-bridge.passthru.fetch-deps
+  dotnetInstallFlags = "-f net8.0";
 
   meta = with lib; {
     homepage = "https://github.com/WiiPlayer2/K8sBridge";
