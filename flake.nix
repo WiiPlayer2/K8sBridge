b418c1677025bca31c8b76d1337eb1e681723565
Waldemar Tomme <waldemar.tomme@bluehands.de>
2025-01-23 12:21:12 +0100

    Add small devShell for publishing a pre-release to nuget.org

--- flake.nix
+++ flake.nix
@@ -34,6 +34,23 @@
               k8s-bridge = pkgs.callPackage ./package.nix {};
               default = k8s-bridge;
             };
+            
+            devShells = rec {
+              default = publishing;
+              publishing = pkgs.mkShell {
+                packages = with pkgs; [
+                  dotnet-sdk_8
+                  dotnetPackages.Nuget
+                  
+                  (writeShellScriptBin "publish-to-nuget" ''
+                    set -xe
+                    dotnet pack ./K8sBridge/K8sBridge.csproj -o packages --version-suffix pre$(date -u +%+4Y%m%d%H%m)
+                    trap "rm ./packages/k8s-bridge.*.nupkg" EXIT
+                    dotnet nuget push ./packages/k8s-bridge.*.nupkg --source nuget.org --skip-duplicate "$@"
+                  '')
+                ];
+              };
+            };            
           };
       }
     );
